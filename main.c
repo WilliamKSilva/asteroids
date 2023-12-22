072d9d4090137485414f565adbb7bb6d87e08148
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-12-22 12:02:24 -0300

    feat: remove absolute path from Spaceship sprite

--- main.c
+++ main.c
@@ -3,17 +3,33 @@
 #include "collision_area.h"
 #include "player.h"
 #include "asteroid.h"
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
+
+// TODO: change from CMake to Makefile
+
+bool OutOfScreen(Vector2 position) {
+  return position.x >= 1920 || position.y >= 1080;
+};
 
 int main()
 {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Asteroids");
   SetTargetFPS(60);
 
-  char *path = "/home/williamkds/Projects/asteroids/assets/spaceship.png";
-  Texture2D spaceshipSprite = LoadTexture(path);
+  char cwd[PATH_MAX];
+  char* path = getcwd(cwd, sizeof(cwd));
+  char *spaceshipSpritePath = strcat(path, "/assets/spaceship.png");
+  
+  if (path == NULL) {
+    perror("Error trying to load meteor asset");
+  }
+
+  Texture2D spaceshipSprite = LoadTexture(spaceshipSpritePath);
 
   Vector2 playerPosition = {SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0};
   Vector2 playerSpeed = {4.0f, 4.0f};
@@ -21,7 +37,6 @@ int main()
   struct CircleCollisionArea collisionArea = NewCollisionArea(playerPosition.x, playerPosition.y, 50, WHITE);
   player->collisionArea = collisionArea;
 
-  // TODO: move Asteroid composition to independent function 
   Vector2 asteroidPosition = {SCREEN_WIDTH / 2.0 + 200, SCREEN_HEIGHT / 2.0};
   Vector2 asteroidSpeed = {2.0f, 2.0f};
   struct Asteroid *asteroid = NewAsteroid(asteroidPosition, asteroidSpeed, BIG, DIAGONAL_LEFT, UP);
