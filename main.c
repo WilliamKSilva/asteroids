dabb8225f7a23c3ad2cef9bee649fd40e45f20ee
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-06-15 21:05:20 -0300

    feat: add logic to render GameObjects with basic textures and Pro textures

--- main.c
+++ main.c
@@ -1,35 +1,116 @@
-#include <stdio.h>
 #include "raylib.h"
+#include <stdio.h>
+
+typedef struct {
+  int centerX;
+  int centerY;
+  float radius;
+  Color color;
+} Circle;
+
+typedef struct {
+  Rectangle sourceRec;
+  Rectangle destRec;
+  Vector2 origin;
+  float rotation;
+} TexturePro;
+
+typedef struct {
+  Vector2 position;
+  Texture2D *sprite;
+  TexturePro *texturePro;
+} GameObject;
+
+void RenderGameObject(GameObject gameObject);
+void MovePlayer(GameObject *player);
 
 int main() {
-  printf("%s", "teste\n");
   const int screenWidth = 1920;
   const int screenHeight = 1080;
 
   InitWindow(screenWidth, screenHeight, "Asteroids");
-  SetTargetFPS(30);
-  while (!WindowShouldClose())    // Detect window close button or ESC key
+  SetTargetFPS(60);
+
+  Texture2D playerSprite = LoadTexture("./assets/player.png");
+  TexturePro texturePro = {
+    {
+      0,
+      0,
+      playerSprite.width,
+      playerSprite.height 
+    },
+    {
+      (float)screenWidth / 2.0,
+      (float)screenHeight / 2.0,
+      playerSprite.width,
+      playerSprite.height 
+    },
+    {
+      playerSprite.width / 2.0,
+      playerSprite.height / 2.0
+    },
+    0
+  };
+
+  GameObject player = {
+      {
+        (float)screenWidth / 2.0,
+        (float)screenHeight / 2.0
+      },
+      &playerSprite,
+      &texturePro
+  };
+
+  while (!WindowShouldClose()) // Detect window close button or ESC key
   {
-      // Update
-      //----------------------------------------------------------------------------------
-      // TODO: Update your variables here
-      //----------------------------------------------------------------------------------
+    // Update
+    //----------------------------------------------------------------------------------
+    // TODO: Update should be framerate independent
+    MovePlayer(&player);
 
-      // Draw
-      //----------------------------------------------------------------------------------
-      BeginDrawing();
-
-          ClearBackground(BLACK);
-
-          DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-      EndDrawing();
-      //----------------------------------------------------------------------------------
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    ClearBackground(BLACK);
+    RenderGameObject(player);
+    EndDrawing();
+    //----------------------------------------------------------------------------------
   }
 
   // De-Initialization
   //--------------------------------------------------------------------------------------
-  CloseWindow();        // Close window and OpenGL context
+  CloseWindow(); // Close window and OpenGL context
   //--------------------------------------------------------------------------------------
   return 0;
+}
+
+void RenderGameObject(GameObject gameObject) {
+  if (gameObject.texturePro) {
+    DrawTexturePro(
+      *gameObject.sprite,
+      gameObject.texturePro->sourceRec,
+      gameObject.texturePro->destRec,
+      gameObject.texturePro->origin,
+      gameObject.texturePro->rotation,
+      WHITE
+    );
+
+    return;
+  }
+
+  DrawTexture(*gameObject.sprite, gameObject.position.x, gameObject.position.y, WHITE);
+}
+
+void MovePlayer(GameObject *player) {
+  if (player->texturePro == NULL) {
+    return;
+  }
+
+  if (IsKeyDown(KEY_A)) {
+    player->texturePro->rotation -= 10;
+  }
+
+  if (IsKeyDown(KEY_D)) {
+    player->texturePro->rotation += 10;
+  }
 }
